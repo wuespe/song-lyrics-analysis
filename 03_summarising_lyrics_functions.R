84eb9892e1a86557329192b8c9e06f1041e22565
wuespe <wojciechgm@gmail.com>
2020-05-14 19:12:43 +0200

    renaming base -> stem

--- 03_summarising_lyrics_functions.R
+++ 03_summarising_lyrics_functions.R
@@ -7,19 +7,6 @@ coalesce = function(x1, x2) {
 }
 
 
-### reading and preparing dictionary  ----
-dict = fread("/home/wojciech/Documents/text_mining/PoliMorf-0.6.7.tab", header = FALSE)
-
-setnames(dict, c("V1", "V2"), c("word", "base"))
-system.time({
-  dict_lite = dict[, .(word = tolower(word), base = tolower(base))] %>% unique
-})
-system.time({
-  setkey(dict_lite, word)
-})
-
-rm(dict)
-
 ### read song and process it to data.table ----
 read_song = function(path) {
   song = readLines(path) %>% 
@@ -36,30 +23,32 @@ join_to_dict = function(dt) {
   dt = dt[, .(.N), keyby=word]
   dt[, word_id := .I]
   # right join on key columns (word==word)
-  dt_tmp = dict_lite[dt, .(word_id, word, base, freq = N)]
-  dt_tmp[, base := coalesce(base, word)]
+  dt_tmp = dict_lite[dt, .(word_id, word, stem, freq = N)]
+  dt_tmp[, in_dict := ifelse(!is.na(stem), 1L, 0L)]
+  dt_tmp[, stem := fcoalesce(stem, word)]
   
   # base forms in frequency (distinct words) order
-  bases_all_sorted = dt_tmp[, .N, base][order(-N), base]
+  stems_all_sorted = dt_tmp[, .N, stem][order(-N), stem]
   # words got duplicated because of multiple base forms 
   # -> aggregate to the word once again and put each word possible bases in a bases_list column
-  dt_ded = dt_tmp[, .(uqN_forms = uniqueN(base),
+  dt_ded = dt_tmp[, .(uqN_forms = uniqueN(stem),
                       freq = freq[1],
-                      bases_list = factor(base, levels = bases_all_sorted) %>% sort(., na.last = F) %>% as.character %>% unique %>% list
+                      in_dict = in_dict[1],
+                      stems_list = factor(stem, levels = stems_all_sorted) %>% sort(., na.last = F) %>% as.character %>% unique %>% list
   ),
   word
   ]
   
   # reduplicate rows to original frequency
   ind_redup = dt_ded[, rep(.I, freq)]
-  dt_ded[ind_redup, .(word, uqN_forms, bases_list)]
+  dt_ded[ind_redup, .(word, uqN_forms, in_dict, stems_list)]
 }
 
 
 # picking most frequent base form - giving lower bound to number of unique words ----
 
 fill_most_freq = function(dt) {
-  dt[, most_freq := sapply(bases_list, . %>% head(., 1L))]
+  dt[, most_freq := sapply(stems_list, . %>% head(., 1L))]
 }
 
 
@@ -67,33 +56,33 @@ fill_most_freq = function(dt) {
 
 fill_least_freq = function(dt, verbose = TRUE) {
   # fill least_freq iteratively
-  # 1) for words that have only 1 base, pick it and go to 2), if there are none END
-  # 2) remove already used base forms from the other words' bases list, then go to 1)
+  # 1) for words that have only 1 stem, pick it and go to 2), if there are none END
+  # 2) remove already used stems from the other words' stems list, then go to 1)
   fill_lf_iter = function(dt1) {
     if(verbose) cat("fill_lf_iter... ", iter)
     while(dt1[, any(uqN_forms==1)]) {
-      counted_base_forms = dt[uqN_forms == 1, do.call(c, bases_list)]
-      dt1[uqN_forms == 1, least_freq := sapply(bases_list, . %>% tail(., 1L))]
+      counted_stems = dt[uqN_forms == 1, do.call(c, stems_list)]
+      dt1[uqN_forms == 1, least_freq := sapply(stems_list, . %>% tail(., 1L))]
       
-      # new base forms list and count
-      dt1[, bases_list := sapply(bases_list, . %>% setdiff(., counted_base_forms))]
-      dt1[, uqN_forms := sapply(bases_list, uniqueN)]
+      # new stems list and count
+      dt1[, stems_list := sapply(stems_list, . %>% setdiff(., counted_stems))]
+      dt1[, uqN_forms := sapply(stems_list, uniqueN)]
       if(verbose) cat("# ")
     }
     if(verbose) cat("\n")
   }
   
-  # if there are no words with 1 base left, check if any have >1 
-  #   for the first such word with least number of bases, pick one base (last one) and do away with the rest
+  # if there are no words with 1 stem left, check if any have >1 
+  #   for the first such word with least number of stems, pick one stem (last one) and do away with the rest
   #   if there are none such words it returns FALSE
   pick_random_form = function(dt1) {
     setkey(dt1, uqN_forms) # sort in ascending order
     idx = dt1[, .(uqN_forms, .I)][uqN_forms > 1, I[1]]
     if(is.na(idx)) return(invisible(FALSE))
     
-    last_val = dt1[idx, unlist(bases_list)] %>% tail(1L)
+    last_val = dt1[idx, unlist(stems_list)] %>% tail(1L)
     if(verbose) cat("pick_random_form: ", dt1[idx, word], " --> ", last_val, "\n")
-    dt1[idx, `:=`(bases_list = list(last_val), uqN_forms = 1L)]
+    dt1[idx, `:=`(stems_list = list(last_val), uqN_forms = 1L)]
     return(invisible(TRUE))
   }
   
@@ -103,6 +92,7 @@ fill_least_freq = function(dt, verbose = TRUE) {
     iter = iter + 1
     fill_lf_iter(dt)
   }
+  dt[, stems_list := NULL]
   sprintf("fill least freq iterations: %d", iter) %>% cat("\n")
 }
 
@@ -116,7 +106,6 @@ join_and_ded = function(dt_songs, verbose = TRUE) {
   system.time({
     fill_least_freq(dt_songs_j, verbose)
   })
-  dt_songs_j[, bases_list := NULL]
   dt_songs_j
 }
 
